1cbec69f74677c7c9461e777564d9ec6988b9a44
Rand0MTutoriaL <thanwisitang7910@gmail.com>
2024-03-21 22:30:34 +0700

    footer 1/3 finished ig

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,10 @@
 import "./page.css";
 import { Intro } from "./components/client/constructor-components";
 import { Article } from "./components/client/utility-components";
+import Client from "./global/client/util";
+
+const { Dynamic } = Client.Components;
+const { Image } = Dynamic;
 
 export default function Home() {
   return (
@@ -17,6 +21,25 @@ export default function Home() {
               </article>
           </section>
       </main>
+      <footer className="footer">
+        <div className="credit-card">
+          <h1>Powered by</h1>
+          <ul>
+            <li><Image dir="icon/" name="react-2.svg" constant alt="react-logo" width={20} height={20}/>&nbsp;<a src="https://react.dev" target="_blank">React</a></li>
+            <li><Image dir="icon/" name="next-js.svg" constant alt="react-logo" width={20} height={20}/>Next.js</li>
+            <li>Vercel</li>
+            <li>Firebase</li>
+          </ul>
+        </div>
+        <div className="credit-card">
+          <h1>Media from</h1>
+          <ul>
+            <li><a href="https://www.flaticon.com/free-icons/search" title="search icons">Search icons created by Catalin Fertu - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/triangle" title="triangle icons">Triangle icons created by Dave Gandy - Flaticon</a></li>
+          </ul>
+        </div>
+        <div className="absolute bottom-0 left-0 w-full h-full light-theme-content-article-fade pt-6 sm:pt-12 dark:h-3/4 dark:dark-theme-footer-gradient"></div>
+      </footer>
       {/* 
       <a href="https://www.flaticon.com/free-icons/search" title="search icons">Search icons created by Catalin Fertu - Flaticon</a> 
       <a href="https://www.flaticon.com/free-icons/triangle" title="triangle icons">Triangle icons created by Dave Gandy - Flaticon</a>

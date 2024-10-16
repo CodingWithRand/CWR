60c9b7e388078630d68cca6840e0150016130fa3
Rand0MTutoriaL <thanwisitang7910@gmail.com>
2024-10-16 11:59:27 +0700

    Thringo project added

--- src/app/my-projects/page.js
+++ src/app/my-projects/page.js
@@ -13,17 +13,7 @@ export default function MyProjects() {
       <Coroussel 
         totalPages={6} 
         corousselElements={[
-          <div key={1} className="intro-page" style={{ backdropFilter: "blur(10px);" }}>
-            <h1 className="title cwr-education">CWR Education</h1>
-            <h2 className="subtitle cwr-education"><i>&quot;A journal based programming learning web application&quot;</i></h2>
-            {/* <p className="development-status">Development Status: Prototype</p> */}
-            <br />
-            <p className="description cwr-education">This project is a web application project that is designed to be a programming learning space. The web theme is in space theme to makes it interesting. This web application is intended to be a Single Page Application (SPA), and is fully developed in React.</p>
-            <br />
-            <a className="visit-site" href="https://cwr-education.vercel.app" target="_blank">Visit Site</a>
-            <a className="learn-more cwr-education" href="https://github.com/CodingWithRand/cwr/tree/general-education" target="_blank">Learn More</a>
-          </div>,
-          <div key={2} className="intro-page" style={{ backdropFilter: "blur(10px) brightness(0.5);" }}>
+          <div key={1} className="intro-page" style={{ backdropFilter: "blur(10px) brightness(0.5);" }}>
             <h1 className="title text-red-600">YouTube Converter</h1>
             <img src="/imgs/backend-images/icon/ytdownloader.ico" alt="YouTube Converter"/>
             <h2 className="subtitle text-white"><i>&quot;Download all your favorite videos as one go!&quot;</i></h2>
@@ -32,14 +22,33 @@ export default function MyProjects() {
             <br />
             <a className="learn-more text-white" href="https://github.com/CodingWithRand/YouTube-Converter" target="_blank">Learn More</a>
           </div>,
-          <div key={3} className="intro-page" style={{ backdropFilter: "blur(5px)" }}>
+          <div key={2} className="intro-page" style={{ backdropFilter: "blur(5px)" }}>
             <h1 className="title text-white">Background Remover</h1>
             <img src="/imgs/backend-images/icon/bg-remover.ico" alt="Background Remover"/>
             <p className="description text-white">This program only runs on PC. It&apos;s a program that allows you to remove the background of an image and download it to your computer. This program is developed in Python and use two engines <b>Pillow</b> which is a module from python and <b>remove.bg</b> which is an API from <a className="text-red-500" href="https://www.remove.bg/" target="_blank">removebg</a></p>
             <br />
             <a className="learn-more text-white" href="https://github.com/CodingWithRand/Background-Remover" target="_blank">Learn More</a>
           </div>,
+          <div key={3} className="intro-page" style={{ backdropFilter: "blur(10px);" }}>
+            <h1 className="title cwr-education">CWR Education</h1>
+            <h2 className="subtitle cwr-education"><i>&quot;A journal based programming learning web application&quot;</i></h2>
+            {/* <p className="development-status">Development Status: Prototype</p> */}
+            <br />
+            <p className="description cwr-education">This project is a web application project that is designed to be a programming learning space. The web theme is in space theme to make it interesting. It is filled with intriguing cutscenes and storytelling. By the way, it&apos;s still in development though. I have just finished the core system such as the registration system and started working on adding the programming content. This web application is intended to be a Single Page Application (SPA) and is fully developed in React.</p>
+            <br />
+            <a className="visit-site" href="https://cwr-education.vercel.app" target="_blank">Visit Site</a>
+            <a className="learn-more cwr-education" href="https://github.com/CodingWithRand/cwr/tree/general-education" target="_blank">Learn More</a>
+          </div>,
           <div key={4} className="intro-page" style={{ backdropFilter: "blur(5px) brightness(0.5);" }}>
+            <h1 className="title thringo"><b>Thringo</b></h1>
+            <h2 className="subtitle thringo"><i>&quot;Got 3 minutes? Learn your English lessons today at Thringo!&quot;</i></h2>
+            <br />
+            <p className="description text-white">A web application project for learning English language. I got an inspiration from <a href="duolingo.com" target="_blank" className="text-lime-500">Duolingo</a>. So, the app's UI might look identical to Duolingo's in some ways. I introduce you to Andy, your English teacher in this app. Right now, there is only one available topic in the app called <i>&quot;Preliminary&quot;</i> which is about word&apos;s suffixes because my teacher assigned that topic for the school work submission. But, I plan to add more topics in the future though. This web application is intended to be a Single Page Application (SPA). It&apos; developed in Next.js, a React framework. Oh, the app is displaying in Thai language by the way.</p>
+            <br />
+            <a className="visit-site" href="https://eng-game.vercel.app" target="_blank">Visit Site</a>
+            <a className="learn-more thringo" href="https://github.com/CodingWithRand/eng-game" target="_blank">Learn More</a>
+          </div>,
+          <div key={5} className="intro-page" style={{ backdropFilter: "blur(5px) brightness(0.5);" }}>
             <h1 className="title text-neutral-800">Simple Survey Form</h1>
             <br />
             <p className="description text-white">A simple survey form to collect opinion from subscribers and viewers on my YouTube channel. The data will be stored in a database, and can be displayed as a table in the admin panel (User: CodingWithRand, Password: cwrcwrcwr). Developed in HTML, CSS, PHP and MySQL</p>
@@ -48,13 +57,6 @@ export default function MyProjects() {
             <a className="visit-site" href="http://codingwithrand.infinityfreeapp.com/Inquiry/" target="_blank">Visit Site</a>
             <a className="learn-more text-white" href="https://github.com/CodingWithRand/cwr-php/tree/inquiry" target="_blank">Learn More</a>
           </div>,
-          <div key={5} className="intro-page" style={{ backdropFilter: "blur(5px) brightness(0.5);" }}>
-            <h1 className="title text-cyan-400">Caesars Cipher <br/>(Rot13 Encryption)</h1>
-            <br />
-            <p className="description text-white">This program is a simple encryption program that allows you to encrypt or decrypt text using the Caesar Cipher. It is developed in JavaScript.</p>
-            <br />
-            <a className="visit-site" href="http://codingwithrand.infinityfreeapp.com/caesar-cipher/" target="_blank">Try now!</a>
-          </div>,
           <div key={6} className="intro-page" style={{ backdropFilter: "blur(10px);" }}>
             <h1 className="title text-white" id="devto-project-name">Camp Activities Inquiry</h1>
             <br />
@@ -62,7 +64,14 @@ export default function MyProjects() {
             <br />
             <a className="visit-site" href="https://codingwithrand.github.io/DEV.to-challenge/" target="_blank">Visit Site</a>
             <a className="learn-more text-white" href="https://dev.to/codingwithrand/glam-up-my-markup-camp-activities-the-frontend-challenge-submission-1m4e" target="_blank">Learn More</a>
-          </div>
+          </div>,
+          <div key={7} className="intro-page" style={{ backdropFilter: "blur(5px) brightness(0.5);" }}>
+            <h1 className="title text-cyan-400">Caesars Cipher <br/>(Rot13 Encryption)</h1>
+            <br />
+            <p className="description text-white">This program is a simple encryption program that allows you to encrypt or decrypt text using the Caesar Cipher. It is developed in JavaScript.</p>
+            <br />
+            <a className="visit-site" href="http://codingwithrand.infinityfreeapp.com/caesar-cipher/" target="_blank">Try now!</a>
+          </div>,
         ]}
         corousselWrappersStyle={[
 
